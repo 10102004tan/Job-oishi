455878d4a4e4a3802c448cea6dc212112dafb369
Nguyen Phuong Tan <tannguyen.10102004@gmail.com>
2024-04-14 14:43:39 +0700

    setup tree project

--- app/src/main/java/com/example/joboishi/Activities/MainActivity.java
+++ app/src/main/java/com/example/joboishi/Activities/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.joboishi;
+package com.example.joboishi.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.joboishi.R;
 
 public class MainActivity extends AppCompatActivity {
 
