95c74ef2eb22525f0bfdf8e7de89a1f9586a9423
Nguyen Phuong Tan <tannguyen.10102004@gmail.com>
2024-04-14 14:45:05 +0700

    done ui my job

--- app/src/main/java/com/example/joboishi/Activities/MainActivity.java
+++ app/src/main/java/com/example/joboishi/Activities/MainActivity.java
@@ -1,16 +1,26 @@
 package com.example.joboishi.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 
+import com.example.joboishi.Adapters.ViewPagerHomeAdapter;
+import com.example.joboishi.Fragments.MyJobFragment;
 import com.example.joboishi.R;
+import com.example.joboishi.databinding.HomeLayoutBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    private HomeLayoutBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = HomeLayoutBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        ViewPagerHomeAdapter adapter = new ViewPagerHomeAdapter(this);
+        binding.fragmentHomeLayout.setOrientation(ViewPager2.ORIENTATION_HORIZONTAL);
+        binding.fragmentHomeLayout.setAdapter(adapter);
     }
 }
