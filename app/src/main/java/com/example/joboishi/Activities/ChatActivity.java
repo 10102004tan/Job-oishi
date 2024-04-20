b647e8b76b0f40d69e209fdf0574af059a0aa3b3
HuynhLyDinhChau <huynhchau.040104@gmail.com>
2024-04-20 16:32:01 +0700

    push profile

--- app/src/main/java/com/example/joboishi/Activities/ChatActivity.java
+++ app/src/main/java/com/example/joboishi/Activities/ChatActivity.java
@@ -6,11 +6,11 @@ import android.os.Bundle;
 
 import com.example.joboishi.R;
 
-public class MainActivity extends AppCompatActivity {
+public class ChatActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_profile);
+        setContentView(R.layout.chat_glints);
     }
 }
