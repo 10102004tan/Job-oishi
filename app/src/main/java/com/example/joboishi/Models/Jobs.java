843c6e47ef0f7811576a42751e196b2614c767a6
Van-Hoang-Dev <vanhoang24edu@gmail.com>
2024-04-30 12:34:37 +0700

    create jobs and comanies model

---
+++ app/src/main/java/com/example/joboishi/Models/Jobs.java
@@ -0,0 +1,18 @@
+package com.example.joboishi.Models;
+
+import java.util.ArrayList;
+
+public class Jobs {
+    private String id;
+    private String title;
+    private Company company;
+    private String requirements;
+    private String responsibilities;
+    private ArrayList<String> skills;
+    private String experience;
+    private int is_edit;
+    private String is_salary_value;
+    private String job_level;
+    private boolean is_applied;
+    private String content;
+}
