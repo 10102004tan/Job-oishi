81573cf08806503f8d931bf6b8d12d2e9c1237f8
HuynhLyDinhChau <huynhchau.040104@gmail.com>
2024-05-14 13:19:08 +0700

    update facebook

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,13 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.google.gms:google-services:4.4.1")
+    }
+}
 plugins {
     id("com.android.application") version "8.1.2" apply false
-    id("com.google.gms.google-services") version "4.4.1" apply false
+//    id("com.google.gms.google-services") version "4.4.1" apply false
 }
