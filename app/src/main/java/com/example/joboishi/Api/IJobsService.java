3f0ecc2081f6f275b8a574db1a86fe22e6aed9c3
Datto098 <dat61222@gmail.com>
2024-05-18 08:01:33 +0700

    chose job criteria in first login

--- app/src/main/java/com/example/joboishi/Api/IJobsService.java
+++ app/src/main/java/com/example/joboishi/Api/IJobsService.java
@@ -1,7 +1,6 @@
 package com.example.joboishi.Api;
 
 import com.example.joboishi.Models.JobBasic;
-import com.example.joboishi.Models.JobSearch;
 
 import java.util.ArrayList;
 
@@ -9,12 +8,12 @@ import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.DELETE;
-import retrofit2.http.GET;
 import retrofit2.http.POST;
 import retrofit2.http.Query;
 
 public interface IJobsService {
-    public static String BASE_URL = "http://10.0.2.2:3001";
+    String BASE_URL = "http://ip172-18-0-43-cp43q3iim2rg00aoeki0-3001.direct.labs.play-with-docker.com/";
+
     @POST("api/jobs")
     Call<ArrayList<JobBasic>> getListJobs(@Query("page") int page);
 
